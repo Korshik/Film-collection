d1428e10583e6980b617f7e6ccfd752ea13bf278
Viktorya Chekunova <vik.korshik.92@gmail.com>
2021-12-02 16:54:04 +0300

    added mock.js

---
+++ js/mock.js
@@ -0,0 +1,5 @@
+const titles=['Jurassic World: Fallen Kingdom'];
+const directors=['J.A. Bayona'];
+const description=['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante ex, vestibulum vitae sem at, dapibus rhoncus nisl. Quisque ut erat et arcu euismod vestibulum.'];
+
+export { titles, directors, description};
