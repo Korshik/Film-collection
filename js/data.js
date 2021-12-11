3176df10debb9d95603750eae0c1b8fb682b4e19
Viktorya Chekunova <vik.korshik.92@gmail.com>
2021-12-11 13:02:33 +0300

    changed data js.

--- js/data.js
+++ js/data.js
@@ -1,18 +1,15 @@
-import { titles, directors, description } from './mock.js';
+import { films } from './mock.js';
+import { getRandomBetween } from './utils';
 
-const createmockData = (ArrLength) => {
-  let array = [...Array(ArrLength).keys()].map(
-    (item, index) =>
-      (item = {
-        titles: titles[index],
-        releaseDate: randomDate('01/01/1990', '01/01/2021') || '-',
-        plot: description[randomInt(0, description.length - 1)] || '-',
-        poster: `./images/posters/${index + 1}.jpg`,
-        boxOffice: randomInt(300000000, 600000000) || 0,
-        rating: random(1, 10).toFixed(1) || 0,
-        directors: directors[index] || '-',
-      })
-  );
+const createmockData = (array) => {
+  let newArray = array.map((item) => {
+    const randomFilm = films[getRandomBetween(0,9)]
+
+    return {
+      ...randomFilm,
+      ...item,
+    }
+  });
   return array;
 };
 export { createmockData };
