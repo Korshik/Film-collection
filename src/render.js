c07099e3198f109d5c3fdb13cc14117f498934e6
Viktorya Chekunova <77636580+Korshik@users.noreply.github.com>
2021-12-21 16:26:50 +0300

    changed render.js

--- src/render.js
+++ src/render.js
@@ -1,27 +1,37 @@
-import{ maxLength }from './utils'
+const filmList = document.createElement('div');
+div.addClassName='film-list'; /**создаю контейнер для наполнения фильмами */
 
-const composeFilmCard = (obj) => {
-  let template=document.getElementById('card-template')
-  let divClone=template.content.cloneNode(true);
-  let {titles,poster, rating, releaseDate, director, plot, boxOffice}=obj;
-  divClone.querySelector('.card-header__title').textContent=titles;
-  divClone.querySelector('.card-header__image').textContent=poster;
-  divClone.querySelector('.film-info__rating .film-info__text').textContent=rating;
-  divClone.querySelector('.film-info__plot .film-info__text').textContent=maxLength(plot);
-  divClone.querySelector('.film-info__release-date .film-info__text').textContent=releaseDate;/**localDateString() */
-  divClone.querySelector('.film-info__director .film-info__text').textContent=director;
-  divClone.querySelector('.film-info__box-office .film-info__text').textContent=`$${boxOffice.toString().match(/.{1,3}/g).join('.')}`;
-  return divClone;
-};
+mokData.forEach((item) => {
+filmList.appendChild(createFilmCard(item));
 
-const renderCard = (filmData) => {
-  const filmList = document.querySelector('.film-list');
-  filmList.innerHTML = '';
-  const cardsItems = [];
-  filmData.forEach((data) => {
-    cardsItems.push(composeFilmCard(data));
-  });
-  filmList.append(...renderCard);
+})
+
+const createFilmCard = (item) => {
+const cardContainer = document.createElement('div');
+cardContainer.addClassName('card');
+
+const cardHeader=document.createElement('div');
+cardHeader.addClassName='card-header';
+cardContainer.appendChild('cardHeader')
+cardHeader.innerHTML= `<img class='card-header__image'>${item.poster}`
+
+const cardTitle = document.createElement('h2');
+cardTitle.addClassName('card-header__title');
+cardTitle.innerHTML = `<h2 class="card-header__title">${item.title}</h2>`;
+cardTitle.after('cardHeader');
+
+const cardBody = document.createElement('div');
+cardBody.addClassName('card__body');
+cardBody.innerHTML+= `<div class="card__body">${item.body}</div>`;
+cardBody.after('cardTitle');
+
+const cardFooter=document.createElement('div');
+cardFooter.addClassName('card__footer');
+const cardButton=document.createElement('button');
+cardButton.addClassName('card__button');
+cardFooter.appendChild('cardButton');
+cardFooter.after('cardBody');
+
+
+return itemContainer
 }
-
-
